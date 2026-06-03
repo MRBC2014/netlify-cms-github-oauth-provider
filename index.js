2ba282e664f84474ed311b98b60cbeda50e7d51f
MRBC2014 <macedonrangesbrewers@gmail.com>
2026-06-03 13:27:59 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const simpleOauthModule = require('simple-oauth2')
-const authMiddleWareInit = require('./auth.js')
-const callbackMiddleWareInit = require('./callback')
-const oauthProvider = process.env.OAUTH_PROVIDER || 'github'
-const loginAuthTarget = process.env.AUTH_TARGET || '_self'
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const CLIENT_ID = process.env.OAUTH_CLIENT_ID;
+const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET;
+const ORIGIN = process.env.ORIGIN || 'https://mrbc2014.github.io';
 
-const config = {
-  client: {
-    id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET
-  },
-  auth: {
-    // Supply GIT_HOSTNAME for enterprise github installs.
-    tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
-    tokenPath: process.env.OAUTH_TOKEN_PATH || '/login/oauth/access_token',
-    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || '/login/oauth/authorize'
+app.get('/auth', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=repo,user`
+  );
+});
+
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+  try {
+    const response = await axios.post(
+      'https://github.com/login/oauth/access_token',
+      { client_id: CLIENT_ID, client_secret: CLIENT_SECRET, code },
+      { headers: { Accept: 'application/json' } }
+    );
+    const token = response.data.access_token;
+    res.send(`
+      <script>
+        window.opener.postMessage(
+          'authorization:github:success:{"token":"${token}","provider":"github"}',
+          '${ORIGIN}'
+        );
+        window.close();
+      </script>
+    `);
+  } catch (err) {
+    res.status(500).send('Auth failed: ' + err.message);
   }
-}
+});
 
-const oauth2 = new simpleOauthModule.AuthorizationCode(config)
-
-function indexMiddleWare (req, res) {
-  res.send(`Hello<br>
-    <a href="/auth" target="${loginAuthTarget}">
-      Log in with ${oauthProvider.toUpperCase()}
-    </a>`)
-}
-
-module.exports = {
-  auth: authMiddleWareInit(oauth2),
-  callback: callbackMiddleWareInit(oauth2, oauthProvider),
-  success: (req, res) => { res.send('') },
-  index: indexMiddleWare
-}
+app.listen(PORT, () => console.log(`OAuth proxy running on port ${PORT}`));
